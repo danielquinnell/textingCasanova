988262476296e01768c5781948b758781cae0c84
Kevin Barthman <kbart897@uwsp.edu>
2013-11-23 20:32:19 -0600

    User account signup
    Adding some fields to the useraccount page

--- useraccount.php
+++ useraccount.php
@@ -1,8 +1,15 @@
 <?php
 	require_once 'conn.php';
 	$userid = '';
-	$name = '';
+	$first_name = '';
+	$last_name = '';
+	$address_1 = '';
+	$address_2 = '';
+	$city = '';
+	$state = '';
+	$zip = '';
 	$email = '';
+	$phone = '';
 	$password = '';
 	$access_lvl = '';
 	if (isset($_GET['userid']))
@@ -30,8 +37,13 @@
 			   
 		$result = mysql_query($sql, $conn)
 			or die ('Could not look up user data: ' . mysql_error());
-			
 		
+		$row = mysql_fetch_array($result);
+		$address_1 = $row['address_line1'];
+		$address_2 = $row['address_line2'];
+		$city = $row['city'];
+		$state = $row['state'];
+		$zip = $row['zip'];
 	}
 	require_once 'header.php';
 	echo '<form method="post" action="transact-user.php">';
@@ -55,51 +67,31 @@
 	Last Name: <br />
     <input type="text" class="textInput" name="last_name" maxlength="100" value="<?php echo htmlspecialchars($last_name); ?>" />
 </p>
+
 <p>
 	Address Line 1: <br />
-    <input type="text" class="textInput" name="address_1" maxlength="100" value="<?php echo htmlspecialchars($address_2); ?>" />
+    <input type="text" class="textInput" name="address_1" maxlength="100" value="<?php echo htmlspecialchars($address_1); ?>" />
 </p>
 <p>
 	Address Line 2: <br />
-    <input type="text" class="textInput" name="address_2" maxlength="100" value="<?php echo htmlspecialchars($address_1); ?>" />
+    <input type="text" class="textInput" name="address_2" maxlength="100" value="<?php echo htmlspecialchars($address_2); ?>" />
 </p>
 <p>
-	Email Address: <br />
-    <input type="text" class="textInput" name="email" maxlength="255" value="<?php echo htmlspecialchars($email); ?>" />
+	City: <br />
+    <input type="text" class="textInput" name="address_2" maxlength="100" value="<?php echo htmlspecialchars($city); ?>" />
 </p>
-<?php
-	if (isset($_SESSION['access_lvl'])
-		and $_SESSION['access_lvl'] == 3)
-	{
-		echo "<fieldset>\n";
-		echo "<legend>Access Level</legend>\n";
-		$sql = "SELECT * " .
-			   "FROM access_levels " .
-			   "ORDER BY access_lvl DESC";
-		$result = mysql_query($sql, $conn)
-			or die('Could not list access levels: ' . mysql_error());
-		while ($row = mysql_fetch_array($result))
-		{
-			echo ' <input type="radio" class="radio id="acl_' .
-				 $row['access_lvl'] . '" name="access_lvl" value="' .
-				 $row['access_lvl'] . '" ';
-			if ($row['access_lvl'] == $access_lvl)
-			{
-				echo 'checked="checked" ';
-			}
-			echo '>' . $row['access_name'] . "<br />\n";
-		}
-?>
-</fieldset>
 <p>
-	<input type="hidden" name="userid" value="<?php echo $userid; ?>"/>
-    <input type="submit" class="submit" name="action" value="Modify Account" />
+	State: <br />
+    <input type="text" class="textInput" name="state" maxlength="2" value="<?php echo htmlspecialchars($state); ?>" />
 </p>
-<?php 
-	}
-	else
-	{
-?>
+<p>
+	Zip: <br />
+    <input type="text" class="textInput" name="zip" maxlength="9" value="<?php echo htmlspecialchars($zip); ?>" />
+</p>
+<p>
+	Phone: <br />
+    <input type="text" class="textInput" name="phone" maxlength="10" value="<?php echo htmlspecialchars($phone); ?>" />
+</p>
 <p>
 	Email Address: <br />
     <input type="text" class="textInput" name="email" maxlength="255" value="<?php echo htmlspecialchars($email); ?>" />
@@ -112,6 +104,47 @@
 	Password (again): <br />
     <input type="password" id="password2" name="password2" maxlength="50" />
 </p>
+<?php
+	if (isset($_SESSION['access_lvl']))
+	{
+?>
+<p>
+	Old Password: <br/>
+    <input type="password" id="old_password" name="old_password" maxlength="50" />
+</p>
+        <?php
+		if ($_SESSION['access_lvl'] == 3)
+		{
+			echo "<fieldset>\n";
+			echo "<legend>Access Level</legend>\n";
+			$sql = "SELECT * " .
+				   "FROM access_levels " .
+				   "ORDER BY access_lvl DESC";
+			$result = mysql_query($sql, $conn)
+				or die('Could not list access levels: ' . mysql_error());
+			while ($row = mysql_fetch_array($result))
+			{
+				echo ' <input type="radio" class="radio id="acl_' .
+					 $row['access_lvl'] . '" name="access_lvl" value="' .
+					 $row['access_lvl'] . '" ';
+				if ($row['access_lvl'] == $access_lvl)
+				{
+					echo 'checked="checked" ';
+				}
+				echo '>' . $row['access_name'] . "<br />\n";
+			}
+			echo '</fieldset>';
+		}
+?>
+<p>
+	<input type="hidden" name="userid" value="<?php echo $userid; ?>"/>
+    <input type="submit" class="submit" name="action" value="Update Account" />
+</p>
+<?php 
+	}
+	else
+	{
+?>
 <p>
 	<input type="submit" class="submit" name="action" value="Create Account" />
 </p>
