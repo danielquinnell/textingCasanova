c0fdc78d51cec83ae939f980e35144ef49f498c0
danielquinnell <quinnelldaniel@gmail.com>
2013-12-16 20:41:30 -0600

    rererererererere conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 	define ('SQL_HOST', 'localhost');
 	define ('SQL_USER', 'root');
-	define ('SQL_PASS', '');
+	define ('SQL_PASS', 'password');
 	define ('SQL_DB', 'wdmd_cartsystem');
 	$conn = mysql_connect(SQL_HOST, SQL_USER, SQL_PASS) or
 		die ('Could not connect to the database: ' . mysql_error());
